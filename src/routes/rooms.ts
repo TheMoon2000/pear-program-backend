75d48307a270684783615462441e504cf0084be9
TheMoon2000 <jiaruishan2000@hotmail.com>
2024-04-20 23:03:31 +0000

    Endpoint updates

--- src/routes/rooms.ts
+++ src/routes/rooms.ts
@@ -19,8 +19,37 @@ async function execAsync(command: string) {
     })
 }
 
-roomRouter.post("/create-server", async (req, res) => {
-    const sessionId: string = req.body?.session_id
+roomRouter.get("/:room_id", async(req, res) => {
+    const userData = await hubInstance.get(`/users/${req.params.room_id}`)
+        .then(r => r.data)
+        .catch(err => {
+            return undefined
+        })
+        
+    if (userData) {
+        // Create temporary token
+        const { token: userToken, id } = await hubInstance.post(`/users/${req.params.room_id}/tokens`, { expires_in: 60 }).then(r => r.data)
+
+        const terminalInfo = await serverInstance.get(`/${req.params.room_id}/api/terminals`, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
+
+        await hubInstance.delete(`/users/${req.params.room_id}/tokens/${id}`)
+
+        res.json({
+            user: userData,
+            terminal: terminalInfo[0]
+        })
+    } else {
+        res.json({ user: null })
+    }
+    
+})
+
+roomRouter.post("/", async (req, res) => {
+
+})
+
+roomRouter.post("/:room_id/create-server", async (req, res) => {
+    const sessionId: string = req.params.room_id
     if (!sessionId) {
         res.status(400).send("Session ID not provided")
     }
@@ -47,8 +76,8 @@ roomRouter.post("/create-server", async (req, res) => {
     }
 })
 
-roomRouter.post("/close-server", async (req, res) => {
-    const sessionId: string = req.body?.session_id
+roomRouter.post("/:room_id/terminate-server", async (req, res) => {
+    const sessionId: string = req.params.room_id
     if (!sessionId) {
         res.status(400).send("Session ID not provided")
     }
@@ -56,39 +85,14 @@ roomRouter.post("/close-server", async (req, res) => {
         await execAsync(`docker exec env deluser ${sessionId}`)
         await execAsync(`docker exec env rm -rf /home/${sessionId}`)
         await hubInstance.delete(`/users/${sessionId}`)
-        res.send("Server closed successfully")
+        res.send("Server terminated successfully")
     } catch (error) {
         return res.status(400).send("Server not opened")
     }
 })
 
-roomRouter.get("/server-status/:sessionId", async(req, res) => {
-    const userData = await hubInstance.get(`/users/${req.params.sessionId}`)
-        .then(r => r.data)
-        .catch(err => {
-            return undefined
-        })
-        
-    if (userData) {
-        // Create temporary token
-        const { token: userToken, id } = await hubInstance.post(`/users/${req.params.sessionId}/tokens`, { expires_in: 60 }).then(r => r.data)
-
-        const terminalInfo = await serverInstance.get(`/${req.params.sessionId}/api/terminals`, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
-
-        await hubInstance.delete(`/users/${req.params.sessionId}/tokens/${id}`)
-
-        res.json({
-            user: userData,
-            terminal: terminalInfo[0]
-        })
-    } else {
-        res.json({ user: null })
-    }
-    
-})
-
-roomRouter.post("/renew/:sessionId", async (req, res) => {
-    const userData = await hubInstance.get(`/users/${req.params.sessionId}`)
+roomRouter.post("/:room_id/restart-server", async (req, res) => {
+    const userData = await hubInstance.get(`/users/${req.params.room_id}`)
         .then(r => r.data)
         .catch(err => {
             return undefined
@@ -96,17 +100,17 @@ roomRouter.post("/renew/:sessionId", async (req, res) => {
     
     if (userData) {
         // Create temporary token
-        const { token: userToken, id } = await hubInstance.post(`/users/${req.params.sessionId}/tokens`, { expires_in: 60 }).then(r => r.data)
+        const { token: userToken, id } = await hubInstance.post(`/users/${req.params.room_id}/tokens`, { expires_in: 60 }).then(r => r.data)
 
-        const terminalInfo = await serverInstance.get(`/${req.params.sessionId}/api/terminals`, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
+        const terminalInfo = await serverInstance.get(`/${req.params.room_id}/api/terminals`, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
 
         if (terminalInfo.length > 0) {
-            await serverInstance.delete(`/${req.params.sessionId}/api/terminals/${terminalInfo[0].name}`, { headers: { Authorization: `token ${userToken}` } })
+            await serverInstance.delete(`/${req.params.room_id}/api/terminals/${terminalInfo[0].name}`, { headers: { Authorization: `token ${userToken}` } })
         }
 
-        const newTerminal = await serverInstance.post(`/${req.params.sessionId}/api/terminals`, undefined, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
+        const newTerminal = await serverInstance.post(`/${req.params.room_id}/api/terminals`, undefined, { headers: { Authorization: `token ${userToken}` } }).then(r => r.data)
 
-        await hubInstance.delete(`/users/${req.params.sessionId}/tokens/${id}`)
+        await hubInstance.delete(`/users/${req.params.room_id}/tokens/${id}`)
 
         res.json({
             user: userData,
@@ -117,15 +121,15 @@ roomRouter.post("/renew/:sessionId", async (req, res) => {
     }
 })
 
-roomRouter.post("/update-code/:sessionId", async (req, res) => {
+roomRouter.post("/:room_id/code", async (req, res) => {
     if (!req.body?.file) {
         return res.status(400).send("Must contain `file` in request body")
     }
 
-    try {
-        fs.writeFileSync(`${req.params.sessionId}.py`, req.body.file)
-        await execAsync(`docker cp ${req.params.sessionId}.py env:/home/${req.params.sessionId}/main.py`)
+    const serializedFile = JSON.stringify(req.body.file)
 
+    try {
+        await execAsync(`docker exec env sh -c 'printf "%s" "$1" > /home/${req.params.room_id}/main.py' sh ${serializedFile}`)
         res.send("File saved")
     } catch (error) {
         console.log(error)
@@ -142,7 +146,6 @@ roomRouter.post("/:room_id/run", async (req, res) => {
             { name: "main.py", content: "x = int(input())\nwhile x < 100:\n    x *= 2\n    print(x)" }
         ]
     })
-    console.log(response)
     res.json(response.data)
 })
 
