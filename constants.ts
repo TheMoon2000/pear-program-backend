e581c100fe253bf8fdcd770b50b849218b85a20a
Regular-ID <78837796+Regular-ID@users.noreply.github.com>
2024-04-21 23:55:13 -0400

    run code/get room info/update room code

--- constants.ts
+++ constants.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 // Root token
 const authHeader = { Authorization: "token 0b1f6b5b4f6246a79d272b15d83f5481" }
-export const hubInstance = axios.create({ baseURL: "http://172.17.0.2:8020/notebook/hub/api", headers: authHeader })
-export const serverInstance = axios.create({ baseURL: "http://172.17.0.2:8020/notebook/user" })
+export const hubInstance = axios.create({ baseURL: "http://172.174.247.133/notebook/hub/api", headers: authHeader })
+export const serverInstance = axios.create({ baseURL: "http://172.174.247.133/notebook/user" })
