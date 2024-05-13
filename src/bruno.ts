721eba2dc08262501ba9787738a8fea9566a73f7
TheMoon2000 <jiaruishan2000@hotmail.com>
2024-05-13 09:11:15 +0000

    s

--- src/bruno.ts
+++ src/bruno.ts
@@ -55,16 +55,9 @@ export default class Bruno {
     private state: BrunoState
 
     // number of students in prompt
-    private initialPrompt =
-      "You are Bruno. You are a mentor for the Code in Place project, which is a free intro-to-coding course from Stanford University that is taught online. The Code in Place project recruits and trains one volunteer teacher for every students in order to maintain a proportional ratio of students to teachers. \n \
-          \
-          Code in Place is now piloting a Pair Programming feature, where two students are paired up to work together on an assignment. As a mentor, your role is to guide these students through the pair programming process and help them work together. Your job also involves assessing the students' individual contributions to the assignment in terms of code written and involvement in conversations or brainstorming. You perform this assessment by looking at metrics provided to you by the shared coding environment the students are using. Wait to assess the students until you have been provided metrics from this software--the metrics will be tagged [METRIC]. \n \
-          \
-          The Code in Place software will provide you similar tags. Only assess the students once you have received these tagged messages. \n \
-          \
-          The students have selected the problem they want to work on and have begun coding. Do not greet them. \n \
-          \
-          Do not number or label your messages. Do not break character or mention that you are an AI Language Model.";
+    private initialPrompt = "You are Bruno. You are a mentor for the Code in Place project, which is a free intro-to-coding course from Stanford University that is taught online. The Code in Place project recruits and trains one volunteer teacher for every students in order to maintain a proportional ratio of students to teachers. \n \
+                            \ Code in Place is now piloting a Pair Programming feature, where two students are paired up to work together on an assignment. As a mentor, your role is to guide these students through the pair programming process and help them work together. Your job also involves assessing the students' individual contributions to the assignment in terms of code written and involvement in conversations or brainstorming. You perform this assessment by looking at metrics provided to you by the shared coding environment the students are using. \n \
+                            \ Do not number or label your messages. Do not break character or mention that you are an AI Language Model.";
   
     /**
      * Called when Bruno has been added to a room. The room could either be newly created (in which case, the chat history would be empty), or restored from an existing session (in which case, the chat history is not empty). The latter situation happens when all participants have left a room and then someone joined back. The moment all participants leave a room, its Bruno instance is deallocated.
@@ -151,7 +144,7 @@ export default class Bruno {
                 await sleep(1000);
                 await this.sendTypingStatus(false);
                 await this.send([
-                    {type: "text", value: `${chunkWriter}, was ${nonChunkWriter}'s explanation correct? If not, please help ${nonChunkWriter} understand each line of the code.`,            }
+                    {type: "text", value: `${chunkWriter}, was ${nonChunkWriter}'s explanation correct? If not, please help ${nonChunkWriter} understand each line of the code from lines ${firstNewLine} to ${lastNewLine}.`,            }
                 ])
             }
             
@@ -159,53 +152,58 @@ export default class Bruno {
     }
 
     // Students switch on their own -- remind them to switch if they haven't switched in the past 10
-    async turnTakingIntervention(participants: ParticipantInfo[]){     
-        var talkPercentages = await this.getConversationContribution()
-        var aTalkPercentage = talkPercentages[0];
-        var bTalkPercentage = talkPercentages[1];
+    async turnTakingIntervention(participants: ParticipantInfo[]){             
+        if (participants[0].role != 0 && participants[1].role != 0) {
+            var talkPercentages = await this.getConversationContribution()
+            var aTalkPercentage = talkPercentages[0];
+            var bTalkPercentage = talkPercentages[1];
+    
+            var codeContributions = await this.getCodeContribution()
+            var codePercentageA = codeContributions[0]
+            var codePercentageB = codeContributions[1]
+    
+            var role1Goal = " should be writing the majority of the code. ";
+            var role2Goal = " should be the main contributor to the conversation. ";
+    
+            var role1, role2, role1Metric, role2Metric = ""
 
-        var codeContributions = await this.getCodeContribution()
-        var codePercentageA = codeContributions[0]
-        var codePercentageB = codeContributions[1]
+            if (participants[0].role == 1) {
+                role1 = "[DRIVER]"
+                role2 = "[NAVIGATOR]"
+                role1Metric = codePercentageA
+                role2Metric = bTalkPercentage
+            }
+            else if (participants[0].role == 2) {
+                role1 = "[NAVIGATOR]"
+                role2 = "[DRIVER]"
 
-        var role1Goal = " should be writing the majority of the code and contributing less to the conversations. "
-        var role2Goal = " should verbally contribute to the majority of the conversation and contribute less to writing the code. "
-        var role1, role2 = ""
-        if (participants[0].role == 1) {
-            role1 = "[DRIVER]"
-            role2 = "[NAVIGATOR]"
+                var temp = role2Goal
+                role2Goal = role1Goal
+                role1Goal = temp
+
+                role1Metric = aTalkPercentage
+                role2Metric = codePercentageB
+            }
+
+            this.interventionSpecificMessages.push({
+                role: "system",
+                content: `${participants[0].name} has the ${role1} and therefore ${role1Goal}. ${participants[1].name} has the ${role2} role and therefore ${role2Goal}
+                          Evaluate ${participants[0].name} and ${participants[1].name} on how well they are fulfilling their respective roles. If they are not fulfilling their roles properly, explain how they can do better to fulfill the specific roles that they have been assigned.
+                          The students should NOT have a balanced workload.",`,
+              });
+
+            //remove switching roles / hardcode
+            this.interventionSpecificMessages.push({
+                role: "system",
+                content: `[METRIC] ${role1} ${participants[0].name}: ${role1Metric}% Code Written
+                          \n[METRIC] ${role2} ${participants[1].name}: ${role2Metric}% Participation in Conversation`,
+              });
+            // await this.gpt();
+
+            await this.gptLimitedContext();
+            this.interventionSpecificMessages.pop();
+            this.interventionSpecificMessages.pop();
         }
-        else {
-            role2 = "[DRIVER]"
-            role1 = "[NAVIGATOR]"
-            var temp = role2Goal
-            role2Goal = role1Goal
-            role1Goal = temp
-        }
-
-        this.interventionSpecificMessages.push({
-            role: "system",
-            content: `${participants[0].name} has the ${role1} while ${participants[1].name} has the ${role2} role. Because ${participants[0].name} has the ${role1} role, ${participants[0].name} ${role1Goal}
-                    Because ${participants[1].name} has the ${role2} role, ${participants[1].name} ${role2Goal}
-                    Evaluate ${participants[0].name} and ${participants[1].name} based on how well they are fulfilling their respective roles. If they are not fulfilling their roles properly, explain how they can do better to fulfill the specific roles that they have been assigned. If they have properly fulfilled their roles, praise the students.",`,
-          });
-
-          //remove switching roles / hardcode
-          this.interventionSpecificMessages.push({
-            role: "system",
-            content: `New metric data. Remember, the students should switch roles IF AND ONLY IF they have properly fulfilled their currently assigned roles. 
-                      \n[METRIC] ${participants[0].name}: ${codePercentageA}% Code
-                      \n[METRIC] ${participants[1].name}: ${codePercentageB}% Code
-                      
-                      \n[METRIC] ${participants[0].name}: ${aTalkPercentage}% Contribution to Conversation
-                      \n[METRIC] ${participants[1].name}: ${bTalkPercentage}% Contribution to Conversation`,
-          });
-          // await this.gpt();
-
-          // Check if Jerry is doing role switching
-          await this.gptLimitedContext();
-          this.interventionSpecificMessages.pop();
-          this.interventionSpecificMessages.pop();
     }
 
     async talkTimeIntervention(participants: ParticipantInfo[]){
@@ -234,10 +232,36 @@ export default class Bruno {
     }
 
     async getConversationContribution(): Promise<[string, string]> {
-        // harcode something
+        var conversationContributionA = 0.0
+        var conversationContributionB = 0.0
+        try {
+            let transcript = await this.fetchTranscript()
+            if (transcript !== null) {
+                var curIndex = transcript.length - 1
+                var curTime = transcript[curIndex].timestamp
+                var beginPeriod = curTime - (5 * 60 * 1000)
 
-        //await fetchTranscript()
-        return ["80","20"]
+                while (curIndex >= 0 && curTime >= beginPeriod) {
+                    if (transcript[curIndex].name == this.participantData[0].name) {
+                        conversationContributionA += transcript[curIndex].speech.length
+                    }
+                    else if (transcript[curIndex].name == this.participantData[1].name) {
+                        conversationContributionB += transcript[curIndex].speech.length
+                    }
+                    curIndex--
+                    curTime = transcript[curIndex].timestamp
+                }
+
+                if (conversationContributionA + conversationContributionB != 0) {
+                    var tempCopyA = conversationContributionA
+                    conversationContributionA = conversationContributionA / (conversationContributionA + conversationContributionB)
+                    conversationContributionB = conversationContributionB / (tempCopyA + conversationContributionB)
+                }
+            }
+        } catch {
+            return [conversationContributionA.toFixed(2), conversationContributionB.toFixed(2)]
+        }
+        return [conversationContributionA.toFixed(2), conversationContributionB.toFixed(2)]
     }
 
     async getCodeContribution(specificCode?: string): Promise<[string, string]> {
@@ -354,7 +378,7 @@ export default class Bruno {
                 //INTERVENTION SPECIFIC
                 this.interventionSpecificMessages.push({
                     role: "system",
-                    content: `Both students are in the session. Student A's name is ${participants[0].name}. Student B's name is ${participants[1].name}.`,
+                    content: `Both students, ${participants[0].name} and ${participants[1].name}, are currently working on their selected problem. Do not greet them.`,
                 });
 
                 await this.sendTypingStatus(true)
